3b10f2f83093dcd83d127c35f3ce729bba257a5f
LeeXTing <selena19820818@gmail.com>
2022-12-15 18:45:54 +0800

    1215

--- 2022_12_01/Assets/Scripts/DialogueSystem.cs
+++ 2022_12_01/Assets/Scripts/DialogueSystem.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using TMPro;
+using System.Collections;
 
 namespace selena
 {
@@ -8,19 +9,93 @@ namespace selena
     /// </summary>
     public class DialogueSystem : MonoBehaviour
     {
+        #region 對話系統
         [SerializeField, Header("對話間隔"), Range(0, 0.5f)]
         private float dialogueIntervalTime = 0.1f;
+        [SerializeField, Header("開頭對話")]
+        private DialogueData dialogueOpening;
+        [SerializeField, Header("對話按鍵")]
+        private KeyCode dialogueKey = KeyCode.Space;
 
-        private WaitForSeconds dialogueInterval;
+        private WaitForSeconds dialogueInterval => new WaitForSeconds(dialogueIntervalTime);
 
         private CanvasGroup groupDialogue;
         private TextMeshProUGUI textName;
         private TextMeshProUGUI textContent;
         private GameObject goTriangle;
+        #endregion
 
+        #region 事件區域
         private void Awake()
         {
-            
+            groupDialogue = GameObject.Find("畫布對話系統").GetComponent<CanvasGroup>();
+            textName = GameObject.Find("對話者名稱").GetComponent<TextMeshProUGUI>();
+            textContent = GameObject.Find("對話內容").GetComponent<TextMeshProUGUI>();
+            goTriangle = GameObject.Find("對話完成圖示");
+            goTriangle.SetActive(false);
+
+            StarDialogue(dialogueOpening);            
+        }
+        #endregion
+
+        public void StarDialogue(DialogueData data)
+        {
+            StartCoroutine(FadeGroup());
+            StartCoroutine(TypeEffect(data));
+        }
+
+        /// <summary>
+        /// 淡入淡出群組物件
+        /// </summary>
+        private IEnumerator FadeGroup(bool fadeIn = true)
+        {
+            // 三元運算子 ? :
+            // 語法 :
+            // 布林值 ? 布林值為 true : 布林值為 false;
+            // true ? 1 : 10; 結果為 1
+            // false ? 1 : 10; 結果為10
+
+            float increase = fadeIn ? +0.1f : -0.1f;
+
+            for (int i = 0; i < 10; i++)
+            {
+                groupDialogue.alpha += increase;
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+
+        /// <summary>
+        /// 打字效果
+        /// </summary>        
+        private IEnumerator TypeEffect(DialogueData data)
+        {
+            textName.text = data.dialogueName;
+
+            for (int j = 0; j < data.dialogueContent.Length; j++)
+            {
+                goTriangle.SetActive(false);
+                textContent.text = "";
+
+                string dialogue = data.dialogueContent[j];
+
+                for (int i = 0; i < dialogue.Length; i++)
+                {
+                    textContent.text += dialogue[i];
+                    yield return dialogueInterval;
+                }
+
+                goTriangle.SetActive(true);
+
+                // 如果 玩家 還沒按下 指定按鍵 就等待
+                while (!Input.GetKeyDown(dialogueKey))
+                {
+                    yield return null;
+                }
+
+                print("<color=#993300>玩家按下按鍵!</color>");
+            }
+
+            StartCoroutine(FadeGroup(false));
         }
     }
 }
---
+++ 2022_12_01/Assets/Scripts/LearnCoroutine.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace selena
+{
+    /// <summary>
+    /// 認識協同程序，簡稱協成 Coroutine
+    /// 目的讓程式停留或等待
+    /// </summary>
+    public class LearnCoroutine : MonoBehaviour
+    {
+        // 使用協同程序的條件
+        // 1. 引用命名空間 System.Collections
+        // 2. 定義一個傳回 IEnumerator 的方法
+        // 3. 方法內必須使用 yield return 時間 停留
+        // 4. 使用 StartCoroutine 啟動
+
+        //字串是一個 char 陣列
+        private string textDialogue = "我是誰?我在哪?";
+
+        private void Awake()
+        {
+            // StartCoroutine(Test());
+
+            // print("第一個字 : " + textDialogue[0]);
+
+            // StartCoroutine(ShowText());
+
+            StartCoroutine(ShowTextUseFor());
+        }
+
+        private IEnumerator Test()
+        {
+            print("<color=#ff3333>第一行</color>");
+            yield return new WaitForSeconds(2);
+            print("<color=#33ff33>第一行</color>");
+            yield return new WaitForSeconds(3.5f);
+            print("<color=#3333ff>第一行</color>");
+        }
+
+        private IEnumerator ShowText()
+        {
+            print(textDialogue[0]);
+            yield return new WaitForSeconds(0.2f);
+            print(textDialogue[1]);
+            yield return new WaitForSeconds(0.2f);
+            print(textDialogue[2]);
+            yield return new WaitForSeconds(0.2f);
+            print(textDialogue[3]);
+        }
+
+        private IEnumerator ShowTextUseFor()
+        {
+            for (int i = 0; i < textDialogue.Length; i++)
+            {
+                print(textDialogue[i]);
+                yield return new WaitForSeconds(0.3f);
+            }
+        }
+    }
+}
